da8eef773fcb763280ed4950c11baf24d0b3bf11
Andi Wiradinata <71416495+andiwiradinata4@users.noreply.github.com>
2024-06-18 22:07:42 +0700

    development on progress

--- ERPS.Infrastructure/obj/Debug/net8.0/ERPS.Infrastructure.AssemblyInfo.cs
+++ ERPS.Infrastructure/obj/Debug/net8.0/ERPS.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPS.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b721add1c06d412f9f56586d9a3d2a4034b76f46")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPS.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPS.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
