93c188ff7cb7b3257b82cca7e6177c341cf9679c
Andi Wiradinata <71416495+andiwiradinata4@users.noreply.github.com>
2024-06-29 18:44:05 +0700

    recommit

--- ERPS.Web/obj/Release/net8.0/ERPS.Web.AssemblyInfo.cs
+++ ERPS.Web/obj/Release/net8.0/ERPS.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPS.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a59a41741b7252c87b06c1d9682e1389bf8c6aa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa7f53ae7376ce5af0d8e09445eee69ed07ddbb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPS.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPS.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
